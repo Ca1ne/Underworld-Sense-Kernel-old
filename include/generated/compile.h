2e5cdc4d7099b107ea45825300851eebb073f136
Ca1ne <Ca1nexda@gmail.com>
2012-03-18 15:52:18 -0400

    fix interactive governor, lol

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 8 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 PREEMPT Mon Mar 12 00:11:25 EDT 2012"
-#define LINUX_COMPILE_TIME "00:11:25"
+#define UTS_VERSION "#8 PREEMPT Sun Mar 18 15:49:41 EDT 2012"
+#define LINUX_COMPILE_TIME "15:49:41"
 #define LINUX_COMPILE_BY "ca1ne"
-#define LINUX_COMPILE_HOST "layer01"
+#define LINUX_COMPILE_HOST "ca1ne-HP-ubuntu"
 #define LINUX_COMPILE_DOMAIN "(none)"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
